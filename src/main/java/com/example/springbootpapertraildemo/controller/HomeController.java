e85957487ebd71021827ec4a5144ee6dbf9b6dd1
Zhi Li <zhi.li@invoicefair.com>
2018-01-17 16:02:54 +0000

    Add Error

--- src/main/java/com/example/springbootpapertraildemo/controller/HomeController.java
+++ src/main/java/com/example/springbootpapertraildemo/controller/HomeController.java
@@ -19,7 +19,7 @@ public class HomeController {
 	
 	@GetMapping("/name")
 	public String getName() {
-		log.warn("Welcome to springboot papertrail", new Exception(new Exception()));
+		log.error("Welcome to springboot papertrail", new Exception(new Exception()));
 		return "Zhi Li";
 	}
 	
