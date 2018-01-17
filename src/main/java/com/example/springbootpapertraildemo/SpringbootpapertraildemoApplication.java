8694b9603040664663c013a76ab94396a55e6941
Zhi Li <zhi.li@invoicefair.com>
2018-01-17 14:51:00 +0000

    RabbmitMQ Test

--- src/main/java/com/example/springbootpapertraildemo/SpringbootpapertraildemoApplication.java
+++ src/main/java/com/example/springbootpapertraildemo/SpringbootpapertraildemoApplication.java
@@ -1,13 +1,20 @@
 package com.example.springbootpapertraildemo;
 
-import javax.xml.ws.RespectBinding;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 
 
@@ -26,4 +33,5 @@ public class SpringbootpapertraildemoApplication {
 		log.warn("Welcome to springboot papertrail", new Exception(new Exception()));
 		return "Hello spring boot papertrail.";
 	}
+	
 }
